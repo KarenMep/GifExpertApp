6325ddccd23fb38197a6b8716e0a9d47a6cacda7
Karen Mep <mepkaren@gmail.com>
2022-11-30 17:46:55 -0600

    useFetchGifs agregado

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,27 @@
-import { getGifs } from "../helpers/getGifs";
+import { useEffect, useState } from "react";
+// import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifItem } from "./GifItem";
 
 export const GifGrid = ({category}) => {
 
-  getGifs(category)
+  const {images, isLoading} = useFetchGifs(category);
+  console.log({isLoading})
 
   return (
     <>
         <h3>{category}</h3>
+
+        <div className='card-grid'>
+          {
+            images.map( (image) => (
+              <GifItem 
+                key={image.id}
+                {...image}
+                />
+            ))
+          }
+        </div>
     </>
   )
 }
--- src/helpers/getGifs.jsx
+++ src/helpers/getGifs.jsx
@@ -1,7 +1,5 @@
-
-
-export const getGifs = async() => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=IL2mTlMnWd6NdAmetbRFbZCMo9AOGhJV&q=${category}&limit=20`
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=IL2mTlMnWd6NdAmetbRFbZCMo9AOGhJV&q=${category}&limit=10`
     const resp = await fetch(url)
     const {data} = await resp.json();
 
@@ -11,6 +9,5 @@ export const getGifs = async() => {
       url: img.images.downsized_medium.url
     }));
 
-    console.log(gifs)
     return gifs;
   }
